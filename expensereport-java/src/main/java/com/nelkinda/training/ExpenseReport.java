54c80f3fb7350e1328267673bc15f0f83e289cc8
Mike Kienenberger <mkienenb@gmail.com>
2022-01-29 16:01:20 -0500

    Reformat code

--- expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
+++ expensereport-java/src/main/java/com/nelkinda/training/ExpenseReport.java
@@ -7,17 +7,17 @@ import java.util.List;
 enum ExpenseCategory {
     MEAL, OTHER
 }
+
 enum ExpenseType {
-    DINNER ("Dinner", ExpenseCategory.MEAL, 5000),
-    BREAKFAST ("Breakfast", ExpenseCategory.MEAL, 1000),
-    CAR_RENTAL ("Car Rental", ExpenseCategory.OTHER, Integer.MAX_VALUE)
-    ;
+    DINNER("Dinner", ExpenseCategory.MEAL, 5000),
+    BREAKFAST("Breakfast", ExpenseCategory.MEAL, 1000),
+    CAR_RENTAL("Car Rental", ExpenseCategory.OTHER, Integer.MAX_VALUE);
 
     private final ExpenseCategory expenseCategory;
     private final int overExpenseAmount;
-    private String reportName;
+    private final String reportName;
 
-    private ExpenseType(String name, ExpenseCategory expenseCategory, int overExpenseAmount) {
+    ExpenseType(String name, ExpenseCategory expenseCategory, int overExpenseAmount) {
         this.expenseCategory = expenseCategory;
         this.overExpenseAmount = overExpenseAmount;
         this.reportName = name;
@@ -51,24 +51,15 @@ public class ExpenseReport {
         parameterizedPrintReport(expenses, new Date(), System.out);
     }
 
-    static class ExpensesCalculation {
-        int total = 0;
-        int mealExpenses = 0;
-    }
-
     public void parameterizedPrintReport(List<Expense> expenses, Date reportDate, PrintStream reportPrintStream) {
         ExpensesCalculation expensesCalculation = calculateExpenses(expenses);
 
         reportPrintStream.println("Expenses " + reportDate);
-
         for (Expense expense : expenses) {
             String expenseName = expense.type.reportName();
-
             String mealOverExpensesMarker = expense.isOverMealExpenseAmount() ? "X" : " ";
-
             reportPrintStream.println(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker);
         }
-
         reportPrintStream.println("Meal expenses: " + expensesCalculation.mealExpenses);
         reportPrintStream.println("Total expenses: " + expensesCalculation.total);
     }
@@ -79,9 +70,13 @@ public class ExpenseReport {
             if (expense.type.isMealExpense()) {
                 expensesCalculation.mealExpenses += expense.amount;
             }
-
             expensesCalculation.total += expense.amount;
         }
         return expensesCalculation;
+    }
+
+    static class ExpensesCalculation {
+        int total = 0;
+        int mealExpenses = 0;
     }
 }
